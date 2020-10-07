7f6a64f05127ebad0e5b0c6c42db04d52a3b1181
Irina-Koksharova <biodgerelo@ukr.net>
2020-10-08 00:09:56 +0300

    Добавляет разметку и функцию изменения класса

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
-const sum = (a, b) => a + b;
-console.log(sum(3, 6));
+import { menuMarkup, menuList, checkbox } from './js/markup';
+import { Theme, oncheckboxClick, favoriteTheme } from './js/toggle';
+
+menuList.insertAdjacentHTML('afterbegin', menuMarkup);
+
+document.body.classList.add(Theme.LIGHT);
+checkbox.addEventListener('change', oncheckboxClick);
+
+favoriteTheme();
